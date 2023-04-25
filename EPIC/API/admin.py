90b4a2869054057adeeb58105f7866da39f07a6d
Tom du Merle <tomdumerle@macbook-pro-de-tom.home>
2023-04-25 10:20:33 +0200

    add of models in the admin.py

--- EPIC/API/admin.py
+++ EPIC/API/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from models import ClientPotentiels, ClientsExistants, Event, Support, Sales, Contract
 # Register your models here.
+
+admin.site.register(ClientPotentiels)
+admin.site.register(ClientsExistants)
+admin.site.register(Event)
+admin.site.register(Support)
+admin.site.register(Sales)
+admin.site.register(Contract)
